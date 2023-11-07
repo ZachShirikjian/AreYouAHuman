eeb6349109712edff598ac151f3b882d93802cb7
TunaYou <zshirikjian@gmail.com>
2023-11-07 17:18:27 -0500

    Player now freezes when time runs out, timer now accurate
    When timer is under 10 seconds timer now says 0:0X instead of 0:X , so timer is more accurate

--- AreYouAHuman/Assets/Scripts/GameManager.cs
+++ AreYouAHuman/Assets/Scripts/GameManager.cs
@@ -6,19 +6,22 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     //VARIABLES//
-    public int timer = 90;    //The # of time (in seconds) a level is.
+    public int timer = 10;    //The # of time (in seconds) a level is.
     private float minutes;
     private float seconds; 
     public TextMeshProUGUI interactText; //text that appears on screen whenever players can pickup an object
     public List<GameObject> playerInventory = new List<GameObject>();
     public TextMeshProUGUI currentStateText;
+    
     //REFERENCES//
     public TextMeshProUGUI timerText; //References the timer text to display how much time is left 
+    public GameObject posePanel; //Reference to the Pose Panel that appears at the end of the level 
+    public PlayerMovement player; //Reference to PlayerMovement script in the Player GameObject
     // Start is called before the first frame update
     //Make sure to initialize (reset) variables EVERY TIME a scene is loaded.
     void Start()
     {
-        timer = 90;
+        timer = 10;
         minutes = Mathf.Floor(timer / 60);
         seconds =  timer - minutes * 60;
         timerText.text = minutes.ToString() + ":" + seconds.ToString();
@@ -26,6 +29,8 @@ public class GameManager : MonoBehaviour
         StartCoroutine(GameTimer());
         interactText.text = "";
         currentStateText.text = "";
+        posePanel.SetActive(false);
+        player.enabled = true;
     }
 
     // Update is called once per frame
@@ -46,9 +51,19 @@ public class GameManager : MonoBehaviour
             timer--;
             minutes = Mathf.Floor(timer / 60);
             seconds =  timer - minutes * 60;
-            timerText.text = minutes.ToString() + ":" + seconds.ToString();
+            if(seconds >= 10)
+            {
+                timerText.text = minutes.ToString() + ":" + seconds.ToString();
+            }
+            if(seconds < 10)
+            {
+                timerText.text = minutes.ToString() + ":0" + seconds.ToString();
+            }
         }
         Debug.Log("Call CheckPose() method.");
+        player.enabled = false;
         currentStateText.text = "TIME'S UP!";
+        yield return new WaitForSeconds(3f);
+        posePanel.SetActive(true);
     }
 }
